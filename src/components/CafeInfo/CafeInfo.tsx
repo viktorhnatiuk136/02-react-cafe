01d32eb3fa5aedf37b5b17802fc921e535ffff0b
viktor hnatiuk <viktorgnatiuk136@gmail.com>
2026-03-22 21:12:34 +0200

    fix CafeInfo component

--- src/components/CafeInfo/CafeInfo.tsx
+++ src/components/CafeInfo/CafeInfo.tsx
@@ -1,11 +1,12 @@
-import css from './CafeInfo.module.css'
+import css from "./CafeInfo.module.css";
 
 export default function CafeInfo() {
-    return (
-        <div className={css.container}>
-            <h1 className={css.title}>Sip Happens Café</h1>
-            <p className={css.description}>
-    Please rate our service by selecting one of the options below.
-            </p>
-        </div>)    
+  return (
+    <div className={css.container}>
+      <h1 className={css.title}>Sip Happens Café</h1>
+      <p className={css.description}>
+        Please rate our service by selecting one of the options below
+      </p>
+    </div>
+  );
 }
